7ae8a2e9c54de0e853bcc74145937eb9c236d9a0
Marcelo de Sena Lacerda <marcelo.lacerda@tjse.jus.br>
2023-11-06 08:28:30 -0300

    Testing migrating to mercury

--- util.pl
+++ util.pl
@@ -1,3 +1,12 @@
 of_type(Variable, CompoundTerm):-
     Variable=CompoundTerm,
     CompoundTerm.
+
+/*
+Check each head of the program
+if head contains a reference to of/2, typecheck that reference,
+
+prepend a call to of_type in the body of the predicate.
+
+check_type(Program).
+*/
